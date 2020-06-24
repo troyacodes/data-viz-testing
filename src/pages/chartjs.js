bab3aa19f0c0b5a8fbd57c556889f3c03d78c96e
Michael <michaeltroya13@gmail.com>
2020-06-24 10:40:31 -0400

    add zoom and pan to chartjs

--- src/pages/chartjs.js
+++ src/pages/chartjs.js
@@ -1,40 +1,49 @@
 import React from "react"
 import { Line } from "react-chartjs-2"
-
 import Layout from "../components/layout"
 
 const Chartjs = () => {
   const data = {
-    labels: ["January", "February", "March", "April", "May"],
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
       {
-        label: "Rainfall",
+        label: "My First dataset",
         fill: false,
-        lineTension: 0.5,
-        backgroundColor: "rgba(75,192,192,1)",
-        borderColor: "rgba(0,0,0,1)",
-        borderWidth: 2,
-        data: [65, 59, 80, 81, 56],
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
       },
     ],
   }
 
+  const options = {
+    pan: {
+      enabled: true,
+      mode: "x",
+    },
+    zoom: {
+      enabled: true,
+      mode: "xy",
+    },
+  }
+
   return (
     <Layout>
-      <Line
-        data={data}
-        options={{
-          title: {
-            display: true,
-            text: "Average Rainfall per month",
-            fontSize: 20,
-          },
-          legend: {
-            display: true,
-            position: "right",
-          },
-        }}
-      />
+      <Line data={data} options={options} />
     </Layout>
   )
 }
